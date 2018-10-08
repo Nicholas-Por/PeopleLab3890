b6d24d61361bfcb1f995b8168a31e099197a3355
Nicholas-Por <nporzio3890@bths.edu>
2018-10-08 19:27:41 -0400

    Teacher.Java

--- src/com/company/Teacher.java
+++ src/com/company/Teacher.java
@@ -1,4 +1,26 @@
 package com.company;
 
-public class Teacher {
+public class Teacher extends Person
+{
+
+    private String Title;
+    private String Subject;
+
+    public Teacher(String firstName, String familyName, String Title, String Subject)
+    {
+        super(firstName,familyName);
+        this.Title = Title;
+        this.Subject = Subject;
+
+    }
+    public String getSubject()
+    {
+        return this.Subject;
+    }
+    public String toString()
+    {
+        return this.Title + this.getFamilyName();
+
+    }
+
 }
